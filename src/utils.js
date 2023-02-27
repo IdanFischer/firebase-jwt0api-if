65cf80efcf91a81e72c3403e2a26790a4d71e8f7
Idan Fischer <Idanfischer5@gmail.com>
2023-02-27 15:09:28 -0500

    securing and verifying in the backend

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,23 @@
+import { initializeApp, cert } from "firebase-admin/app"
+import { getAuth } from "firebase/auth"
+import { creds } from "./secrets.js"
 
 
-export function getSecrets(req, res) {
-  res.send({message: "The baby eagle sits in the nest at night."})
+export async function getSecrets(req, res) {
+  // first check if they are sending a token
+  if (!req.hedaers || !req.headers.authorization) {
+    // can also do req.hedaers && req.headers.authorization
+    res.status(403).send({ message: "Forbidden" })
+    return
+  }
+  // connect to firebase
+  initializeApp({
+    credential: cert(creds)
+  })
+  const decoded = await getAuth().verifyIdToken(req.headers.authorization)
+  if (!decoded) {
+    res.status(403).send({ message: "Forbidden"})
+  }
+  // VALIDATE TOKEN
+  res.send({ message: "The baby eagle sits in the nest at night." })
 }
